70c70e5af00ba9bdb7f9edda3481d8297edacc63
Christian Steia <christian_steia@yahoo.com>
2024-01-25 18:38:27 +0200

    created user context

--- src/components/sign-in-form/sign-in-form.component.js
+++ src/components/sign-in-form/sign-in-form.component.js
@@ -25,15 +25,14 @@ const SignInForm = () => {
   }
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await signInUserWithEmailAndPassword(email, password);
+      await signInUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch(error) {
       switch (error.code) {
