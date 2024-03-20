30e1962ff42897ed5ea85e2ac6d4f88f9c838503
Christian Steia <christian_steia@yahoo.com>
2024-03-20 13:35:54 +0200

    converted to progressive web app

--- src/components/directory-item/directory-item.styles.tsx
+++ src/components/directory-item/directory-item.styles.tsx
@@ -1,14 +1,16 @@
-import styled from'styled-components';
+import styled from 'styled-components';
 
-export const BackgroundImage = styled.div<{ imageUrl: string }>(
-  ({ imageUrl }) => ({
-    width: '100%',
-    height: '100%',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundImage: `url(${imageUrl})`,
-  })
-);
+type BackgroundImageProps = {
+  imageurl: string;
+};
+
+export const BackgroundImage = styled.div<BackgroundImageProps>`
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-image: ${({ imageurl }) => `url(${imageurl})`};
+`;
 
 export const Body = styled.div`
   height: 90px;
@@ -47,24 +49,28 @@ export const DirectoryItemContainer = styled.div`
   margin: 0 7.5px 15px;
   overflow: hidden;
 
-  &:hover {
-    cursor: pointer;
-
-    & ${BackgroundImage} {
-      transform: scale(1.1);
-      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-    }
-
-    & ${Body} {
-      opacity: 0.9;
-    }
-  }
-
   &:first-child {
     margin-right: 7.5px;
   }
 
   &:last-child {
     margin-left: 7.5px;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+
+    ${Body} {
+      opacity: 0.9;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    height: 200px;
   }
 `;
